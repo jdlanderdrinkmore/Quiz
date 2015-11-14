c4b7354fc4f95006f80489b9d7aee618fa81f9ef
jdlanderdrinkmore <jdlander@jdlanderdeMacBook-Pro.local>
2015-11-14 14:06:30 +0800

    Another comment add

--- Quiz/main.m
+++ Quiz/main.m
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 //add a comment
+//add another comment
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
